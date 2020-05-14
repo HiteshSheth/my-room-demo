2d2d52b66ce58995c5a9a2bdcd7c0e6d429c60a3
Room-Db <shethhitesh63@gmail.com>
2020-05-14 17:48:42 +0530

    Add new line in AddActivity file.

--- app/src/main/java/com/hitesh/myapplication/AddActivity.kt
+++ app/src/main/java/com/hitesh/myapplication/AddActivity.kt
@@ -14,6 +14,7 @@ class AddActivity : AppCompatActivity() {
 
     //This is demo
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
